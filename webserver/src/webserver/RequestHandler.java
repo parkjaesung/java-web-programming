4e29898e066732b1581a46aef084b6a3c7adc617
javajigi <javajigi@gmail.com>
2014-08-24 15:29:41 +0900

    웹 서버를 단계적으로 구현할 수 있도록 예제 코드 구현함.

--- webserver/src/webserver/RequestHandler.java
+++ webserver/src/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -15,9 +16,34 @@ public class RequestHandler extends Thread {
 	public void run() {
 		System.out.println("New Client Connected!");
 		
-		try (InputStream is = connection.getInputStream(); 
-			 OutputStream os = connection.getOutputStream()) {
+		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 			
+			DataOutputStream dos = new DataOutputStream(out);
+			byte[] body = "Hello World".getBytes();
+			response200Header(dos, body.length);
+			responseBody(dos, body);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private void response200Header(DataOutputStream dos, int lengthOfBytes) {
+		try {
+			dos.writeBytes("HTTP/1.0 200 Document Follows \r\n");
+			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + lengthOfBytes + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	private void responseBody(DataOutputStream dos, byte[] body) {
+		try {
+			dos.write(body, 0, body.length);
+			dos.writeBytes("\r\n");
+			dos.flush();
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
---
+++ webserver/test/util/PathUtilsTest.java
@@ -0,0 +1,12 @@
+package util;
+
+import org.junit.Test;
+
+public class PathUtilsTest {
+
+	@Test
+	public void getCurrentPath() {
+		System.out.println("CurrentPath : " + PathUtils.getCurrentPath());
+	}
+
+}
