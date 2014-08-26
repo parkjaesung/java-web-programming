088aa594d34a857e819266f72be64641af2fede3
javajigi <javajigi@gmail.com>
2014-08-26 19:28:10 +0900

    Servlet init 메서드에서 초기화하도록 수정함.

--- qna/src/main/java/core/nmvc/SpringMvcController.java
+++ qna/src/main/java/core/nmvc/SpringMvcController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.servlet.LocaleResolver;
@@ -19,22 +20,29 @@ import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import core.utils.ServletContextUtils;
-
 public class SpringMvcController extends HttpServlet {
 	private static final long serialVersionUID = -5704524104722380068L;
 	private static final Logger logger = LoggerFactory.getLogger(SpringMvcController.class);
+	
+	private ConfigurableWebApplicationContext context;
+	private BeanNameUrlHandlerMapping handlerMapping;
+	
+	@Override
+	public void init() throws ServletException {
+		context = new XmlWebApplicationContext();
+		context.setConfigLocation(configPath());
+		context.setServletContext(getServletContext());
+		context.setServletConfig(getServletConfig());
+		context.refresh();
+		
+		handlerMapping = new BeanNameUrlHandlerMapping();
+		handlerMapping.setApplicationContext(context);
+	}
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		logger.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
-		XmlWebApplicationContext context = new XmlWebApplicationContext();
-		context.setConfigLocation(configPath());
-		context.setServletContext(ServletContextUtils.getServletContext(request));
-		context.refresh();
-		BeanNameUrlHandlerMapping handlerMapping = new BeanNameUrlHandlerMapping();
-		handlerMapping.setApplicationContext(context);
 		
 		try {
 			HandlerExecutionChain hec = handlerMapping.getHandler(request);
