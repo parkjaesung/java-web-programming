b0c849d3e26a4b474dc984a6c92fa148873da9c3
javajigi <javajigi@gmail.com>
2014-09-16 10:37:45 +0900

    댓글 수 증가 시키는 부분을 분리함.

--- qna/src/main/java/next/api/controller/AddAnswerController.java
+++ qna/src/main/java/next/api/controller/AddAnswerController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import next.dao.AnswerDao;
+import next.dao.QuestionDao;
 import next.model.Answer;
 import core.mvc.Controller;
 import core.utils.ApplicationContextUtils;
@@ -13,14 +14,16 @@ public class AddAnswerController implements Controller {
 	@Override
 	public String execute(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		AnswerDao dao = ApplicationContextUtils.getBean(request, AnswerDao.class);
+		AnswerDao answerDao = ApplicationContextUtils.getBean(request, AnswerDao.class);
+		QuestionDao questionDao = ApplicationContextUtils.getBean(request, QuestionDao.class);
 		
 		long questionId = Long.parseLong(request.getParameter("questionId"));
 		String writer = request.getParameter("writer");
 		String contents = request.getParameter("contents");
 		Answer answer = new Answer(writer, contents, questionId);
 
-		dao.insert(answer);
+		answerDao.insert(answer);
+		questionDao.updateCommentCount(questionId);
 
 		return Controller.DEFAULT_API_PREFIX;
 	}
